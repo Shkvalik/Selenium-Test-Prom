c773c5387866bd6c2d4c5130561a15dc04c4f67f
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-03 15:32:39 +0200

    Add first locator for main page

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    NAME_CATEGORY = (By.CSS_SELECTOR, "h1[class='_3h93n _21mbo IecKS _1VPQi _2Wxbt'] span[class='_3h93n']")
