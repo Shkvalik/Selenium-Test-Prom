8b961eb8fe9bf56474cff4043ef5204ac721ff17
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-04 17:39:42 +0200

    Set window size for testing window

--- conftest.py
+++ conftest.py
@@ -2,10 +2,11 @@
 import pytest
 from selenium import webdriver
 
-
 @pytest.fixture(scope="function")
 def browser():
+    browser = webdriver.Chrome()
     browser.implicitly_wait(10)
+    browser.set_window_size(1600, 800)
     yield browser
     print("\nquit browser..")
     browser.quit()
