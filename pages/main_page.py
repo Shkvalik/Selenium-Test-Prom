469e35d353dcd151ef31665372d54ca4021922df
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-06 01:38:39 +0200

    Switch method with heart to main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,14 +3,18 @@ from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
+    def heart_is_filled_with_color(self):
+        heart = self.browser.find_element(*MainPageLocators.HEART).get_attribute('d')
+        assert 'M10' in heart
+
+    def heart_is_not_filled_with_color(self):
+        heart = self.browser.find_element(*MainPageLocators.HEART).get_attribute('d')
+        assert 'M14.5' in heart
+
     def should_be_correct_name_category(self):
         assert MainPageLocators.PART_NAME_CATEGORY_URL in self.browser.current_url, f"Current url is incorrect --> {self.browser.current_url}"
 
     def add_goods_to_favorites(self):
-        button = self.browser.find_element(*MainPageLocators.BUTTON_ADD_TO_FAVORITES)
-        button.click()
-
-    def remove_goods_from_favorites(self):
         button = self.browser.find_element(*MainPageLocators.BUTTON_ADD_TO_FAVORITES)
         button.click()
 
