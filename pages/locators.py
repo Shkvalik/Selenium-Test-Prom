e01a38afade5fb3117d76ed33b7e9e02ae530e83
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-05 23:15:39 +0200

    Initialization locators for favorites page

--- pages/locators.py
+++ pages/locators.py
@@ -2,17 +2,24 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators():
-    BUTTON_FAVORITE = (By.CSS_SELECTOR, "button[aria-label='Избранное']")
+    BUTTON_FAVORITES = (By.CSS_SELECTOR, "button[aria-label='Избранное']")
     BUTTON_LOGIN = (By.CSS_SELECTOR, "button[class='_2KaCs']")
     BUTTON_LOGIN_BY_EMAIL = (By.CSS_SELECTOR, "div[data-qaid='email_btn']")
     INPUT_LOGIN = (By.CSS_SELECTOR, "input[id='email_field']")
     INPUT_PASSWORD = (By.CSS_SELECTOR, "input[id='enterPassword']")
     BUTTON_CONFIRM_INPUT_LOGIN = (By.CSS_SELECTOR, "button[id='emailConfirmButton']")
     BUTTON_CONFIRM_INPUT_PASSWORD = (By.CSS_SELECTOR, "button[id='enterPasswordConfirmButton']")
-    COUNTER_FAVORITE_GOODS = (By.CSS_SELECTOR, "div[class='_1Ol4v']")
+    COUNTER_FAVORITES_GOODS = (By.CSS_SELECTOR, "div[data-qaid='counter']")
     BUTTON_CABINET = (By.CSS_SELECTOR, "button[data-qaid='menu_btn']")
 
+
 class MainPageLocators():
-    NAME_CATEGORY = 'Velosipednye-shiny'
-    BUTTON_ADD_TO_FAVORITE = (By.CSS_SELECTOR, "div[class='GWc_y _3V5Rw _1u0_W'] div[class='yJNOx']")
-    HEART = (By.XPATH, "//*[local-name() = 'path']")
+    PART_NAME_CATEGORY_URL = 'Velosipednye-shiny'
+    BUTTON_ADD_TO_FAVORITES = (By.CSS_SELECTOR, "div[class='GWc_y _3V5Rw _1u0_W'] div[class='yJNOx']")
+    HEART = (By.XPATH, "//div[@class='GWc_y _3V5Rw _1u0_W']/div[@class='yJNOx']//*[local-name() = 'path']")
+
+
+class FavoritesPageLocators():
+    PART_FAVORITES_URL = 'favorites'
+    FAVORITES_GOODS = (By.CSS_SELECTOR, "div[data-qaid='favorite_row']")
+    BUTTON_DELETE_GOODS = (By.CSS_SELECTOR, "span[data-qaid='delete_icon']")
