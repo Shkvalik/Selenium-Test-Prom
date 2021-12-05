449f280f873aea76e87d5848244bcf43d5cbf7fd
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-05 23:14:08 +0200

    Initialization new tests and fixture for clean

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,55 @@
 import pytest
 from selenium import webdriver
+
+from pages.favorites_page import FavoritesPage
 from pages.main_page import MainPage
+
+
+class TestInteractionsWithProduct():
+    @pytest.fixture(scope="function", autouse=True)
+    def clean(self, browser):
+        link = 'https://prom.ua/Velosipednye-shiny'
+        page = MainPage(browser, link)
+        page.open()
+        page.login()
+        page.should_be_authorized_user()
+        page.go_to_favorites_page()
+
+        page = FavoritesPage(browser, browser.current_url)
+        page.goods_must_be_in_favorites()
+        page.remove_goods_from_favorites()
+        page.musnt_be_counter_favorites_goods()
+
+    def test_user_can_add_goods_to_favorites(self, browser):
+        link = 'https://prom.ua/Velosipednye-shiny'
+        page = MainPage(browser, link)
+        page.open()
+        page.login()
+        page.should_be_authorized_user()
+        page.heart_is_not_filled_with_color()
+        page.musnt_be_counter_favorites_goods()
+        page.add_goods_to_favorites()
+        page.must_be_counter_favorites_goods()
+
+    def test_user_can_see_goods_in_favorites(self, browser):
+        link = 'https://prom.ua/Velosipednye-shiny'
+        page = MainPage(browser, link)
+        page.open()
+        page.heart_is_not_filled_with_color()
+        page.add_goods_to_favorites()
+        page.heart_is_filled_with_color()
+        page.must_be_counter_favorites_goods()
+        page.go_to_favorites_page()
+
+        page = FavoritesPage(browser, browser.current_url)
+        page.goods_must_be_in_favorites()
+        page.must_be_counter_favorites_goods()
+
+    def test_user_cant_see_goods_in_favorites(self, browser):
+        link = 'https://prom.ua/Velosipednye-shiny'
+        page = FavoritesPage(browser, link)
+        page.favorites_must_be_empty()
+        page.musnt_be_counter_favorites_goods()
 
 
 def test_user_be_in_the_correct_page(browser):
@@ -10,12 +59,3 @@ def test_user_be_in_the_correct_page(browser):
     page.should_be_correct_name_category()
 
 
-def test_user_can_add_goods_to_favorite(browser):
-    link = 'https://prom.ua/Velosipednye-shiny'
-    page = MainPage(browser, link)
-    page.open()
-    page.login()
-    page.should_be_authorized_user()
-    page.add_good_to_favorites()
-    page.heart_is_filled_with_color()
-    page.should_be_counter_favorite_goods()
