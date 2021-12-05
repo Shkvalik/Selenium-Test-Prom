57de917541098d34988e3e3680c42047234fc383
Shkvalik <wal2004yandex.ru@gmail.com>
2021-12-05 23:14:52 +0200

    Initialization method for remove goods

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,13 @@ from pages.locators import MainPageLocators
 
 class MainPage(BasePage):
     def should_be_correct_name_category(self):
-        assert MainPageLocators.NAME_CATEGORY in self.browser.current_url, f"Current url is incorrect --> {self.browser.current_url}"
+        assert MainPageLocators.PART_NAME_CATEGORY_URL in self.browser.current_url, f"Current url is incorrect --> {self.browser.current_url}"
 
-    def add_good_to_favorites(self):
-        button = self.browser.find_element(*MainPageLocators.BUTTON_ADD_TO_FAVORITE)
+    def add_goods_to_favorites(self):
+        button = self.browser.find_element(*MainPageLocators.BUTTON_ADD_TO_FAVORITES)
         button.click()
+
+    def remove_goods_from_favorites(self):
+        button = self.browser.find_element(*MainPageLocators.BUTTON_ADD_TO_FAVORITES)
+        button.click()
+
